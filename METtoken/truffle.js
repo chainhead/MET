d7c9162a22393db830f818f88cf858295356c1f9
chainhead <mrchainhead@gmail.com>
2018-03-24 10:17:42 +0530

    Initial commit

--- METtoken/truffle.js
+++ METtoken/truffle.js
@@ -1,13 +1,4 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  networks: {
-    ganache: { 
-      network_id: "*", 
-      host: "localhost",
-      port: 8545,
-      gas: 4700000,
-      gasPrice: 100000000000,
-    }
-  }
 };
