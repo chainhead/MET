c1f816573b0970b494cb8ad0edaca278563c933f
chainhead <mrchainhead@gmail.com>
2018-03-27 05:04:32 +0530

    Working with faucet.

--- METtoken/migrations/1_initial_migration.js
+++ METtoken/migrations/1_initial_migration.js
@@ -1,5 +1,12 @@
+const Web3 = require('web3');
+const TruffleConfig = require('../truffle');
+const config = TruffleConfig.networks.aws;
+
 var Migrations = artifacts.require("./contracts/Migrations.sol");
 
 module.exports = function(deployer) {
+  const web3 = new Web3(new Web3.providers.HttpProvider('http://' + config.host + ':' + config.port));
+  web3.personal.unlockAccount(config.from, 'Welc0me', 36000);
+
   deployer.deploy(Migrations);
 };
--- METtoken/truffle.js
+++ METtoken/truffle.js
@@ -1,10 +1,28 @@
+/** 
+require('dotenv').config();
+const Web3 = require("web3");
+const web3 = new Web3();
+const WalletProvider = require("truffle-wallet-provider");
+const Wallet = require('ethereumjs-wallet');
+
+
+var mainNetPrivateKey = new Buffer(process.env["MAINNET_PRIVATE_KEY"], "hex")
+var mainNetWallet = Wallet.fromPrivateKey(mainNetPrivateKey);
+var mainNetProvider = new WalletProvider(mainNetWallet, "https://mainnet.infura.io/");
+
+var ropstenPrivateKey = new Buffer(process.env["ROPSTEN_PRIVATE_KEY"], "hex")
+var ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
+var ropstenProvider = new WalletProvider(ropstenWallet, "https://ropsten.infura.io/");
+*/
+
 module.exports = {
 	networks: {
 		aws: { // Private network on AWS
 			network_id: "15",
-			host: "54.175.174.197",
+			host: "54.90.69.86",
 			port: 8545,
-			gas: 6721975
+			gas: 9721975,
+			from: "0x9afb7463ae8b42b4ab8c9434ecce5e9673cdcbbf"
 		}
 	}
 };
